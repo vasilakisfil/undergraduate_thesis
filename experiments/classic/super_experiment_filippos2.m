6c7f4a842f40c42b1456976828979f3aa1f6190b
Filippos Vasilakis <vasilakisfil@gmail.com>
2012-07-15 15:06:09 +0300

    Finished 2nd chapter. I am going to merge chapters 4 and 5 in one chapter.

---
+++ experiments/classic/super_experiment_filippos2.m
@@ -0,0 +1,28 @@
+function [] = super_experiment_filippos2( )
+
+clear;
+clc;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%EXPERIMENTS SETTINGS%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+network_radius = 50;
+noOfNodes = 1000;
+noOfEvents = 5000;
+sensors_charging_number = 10;
+charger_percentage = 0.4;
+
+energy_c = 2;
+range_c = 2.3;
+communication_range = network_radius*sqrt((log(noOfNodes))/(pi*noOfNodes));
+communication_range_greedy = range_c*network_radius*sqrt((log(noOfNodes))/(pi*noOfNodes));
+total_energy_1 = 4*noOfEvents*communication_range*network_radius/2;
+total_energy_2 = energy_c/3*noOfEvents*(network_radius^2)/2;
+
+repetitions = 20;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+experiment_2_full_vs_partial(network_radius, noOfNodes, noOfEvents, sensors_charging_number, charger_percentage, communication_range, communication_range_greedy, total_energy_1, total_energy_2, repetitions);
+
+
+
+
